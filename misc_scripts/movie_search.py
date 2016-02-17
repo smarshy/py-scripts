a041c0601d273657292889082c05f7c716599f8d
vatsala <vatsala.swaroop@gmail.com>
2016-02-17 16:27:00 +0530

    Added comments

--- misc_scripts/movie_search.py
+++ misc_scripts/movie_search.py
@@ -2,6 +2,11 @@ import sys
 import urllib2
 import json 
 
+#sample usage : python movie_search.py batman 2
+#sample usage : python movie_search.py batman 
+#First argument is search keyword and second is page number
+
+#gets arguments from terminal
 def get_arguments():
 	if len(sys.argv) is 3:
 		return {'keyword': sys.argv[1], 'page': sys.argv[2]}
@@ -11,6 +16,7 @@ def get_arguments():
 		print 'No argument given!'
 		sys.exit()
 
+#gets url depending on arguments
 def get_url():
 	args = get_arguments()
 	if args['page']:
@@ -19,18 +25,20 @@ def get_url():
 		url = "http://www.omdbapi.com/?s=" + str(args['keyword'])
 	return url
 
+#displays resultant data
 def display_data(results) :
-	print "Returned search resuts are: (in the format - movie name, imdb id, type, year )"
+	print "Returned search resuts are: (in the format - movie name, imdb id, type, year )\n"
 	for result in results:
 		print result['Title'] + "\t" + result['imdbID'] + "\t" + result['Year'] + "\t" + result['Type']
 
+#main function
 if __name__ == '__main__':
 
 	url = get_url()
 	data = json.load(urllib2.urlopen(url))
 
 	if data['Response']:
-		print "Displaying" + str(len(data['Search'])) + " of total " + str(data['totalResults']) +" results "
+		print "\nDisplaying " + str(len(data['Search'])) + " of total " + str(data['totalResults']) +" results \n"
 		display_data(data['Search']) 
 	else:
 		print "Sorry. This page is not available"
