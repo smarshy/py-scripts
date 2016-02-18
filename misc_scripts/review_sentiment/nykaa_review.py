fddd501ad7ac74a236df820d972e1a0b464b950b
vatsala <vatsala.swaroop@gmail.com>
2016-02-18 10:53:21 +0530

    Added sentiment map from the files :ok_hand:

--- misc_scripts/review_sentiment/nykaa_review.py
+++ misc_scripts/review_sentiment/nykaa_review.py
@@ -1,7 +1,10 @@
 import sys
 import urllib2
+import string
 from bs4 import BeautifulSoup as bs
 
+POSITIVE_FILE = "/home/vatsala/py-scripts/misc_scripts/review_sentiment/positive.txt"
+NEGATIVE_FILE = "/home/vatsala/py-scripts/misc_scripts/review_sentiment/negative.txt"
 
 def extract_reviews(url):
     html_doc = urllib2.urlopen(url)
@@ -10,13 +13,26 @@ def extract_reviews(url):
     review_content = [str(review.findAll('p')[1].get_text()) for review in soup.findAll('div', class_ ='r-detail')]
     return {'review_head': review_head, 'review_content': review_content}
 
+def get_file_data(filename):
+    inFile = open(filename, 'r', 0)
+    line = inFile.readline()
+    wordlist = string.split(line)
+    inFile.close()
+    return wordlist
+
+def get_sentiment_list():   
+    print "senti"
+    positive = get_file_data(POSITIVE_FILE)
+    negative = get_file_data(NEGATIVE_FILE)
+    return {'positive': positive , 'negative': negative}
 
 if __name__ == '__main__':
     url = 'http://www.nykaa.com/bath-and-body/nivea-body-lotion-extra-whitening-cell-repair-uv-protect-vit-c.html?root=catg_Nykaas%20Choice&ptype=product&brand=catg_nykaas+choice'
     try:
         reviews = extract_reviews(url)
         if len(reviews['review_head']) != 0:
-            print "not empty"
+            sentiment_map = get_sentiment_list()
+            print sentiment_map
         else:
             print "Either this product has no reviews or this is not a valid product page"
     except:
