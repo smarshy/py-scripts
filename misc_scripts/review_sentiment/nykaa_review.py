c81c55185d28afe6756a7303cfd2154fe2aa0104
vatsala <vatsala.swaroop@gmail.com>
2016-02-18 11:44:42 +0530

    Restructuring a bit of code

--- misc_scripts/review_sentiment/nykaa_review.py
+++ misc_scripts/review_sentiment/nykaa_review.py
@@ -6,9 +6,7 @@ from bs4 import BeautifulSoup as bs
 POSITIVE_FILE = "/home/vatsala/py-scripts/misc_scripts/review_sentiment/positive.txt"
 NEGATIVE_FILE = "/home/vatsala/py-scripts/misc_scripts/review_sentiment/negative.txt"
 
-def extract_reviews(url):
-    html_doc = urllib2.urlopen(url)
-    soup = bs(html_doc.read(), 'html.parser')
+def extract_reviews(soup):
     review_head = [str(review.findAll('p')[0].get_text()) for review in soup.findAll('div', class_ ='r-detail')]
     review_content = [str(review.findAll('p')[1].get_text()) for review in soup.findAll('div', class_ ='r-detail')]
     return {'review_head': review_head, 'review_content': review_content}
@@ -62,14 +60,19 @@ def find_sentiment(reviews, sentiment_map):
 
 if __name__ == '__main__':
     url = 'http://www.nykaa.com/bath-and-body/nivea-body-lotion-extra-whitening-cell-repair-uv-protect-vit-c.html?root=catg_Nykaas%20Choice&ptype=product&brand=catg_nykaas+choice'
+    
     try:
-        reviews = extract_reviews(url)
+        html_doc = urllib2.urlopen(url)
+        soup = bs(html_doc.read(), 'html.parser')
+        reviews = extract_reviews(soup)
+
         if len(reviews['review_head']) != 0:
             sentiment_map = get_sentiment_list()
             print
-            print " Recent reviews are largely " + find_sentiment(reviews, sentiment_map) + " in sentiment"
+            print "Recent reviews are largely " + find_sentiment(reviews, sentiment_map) + " in sentiment"
         else:
             print "Either this product has no reviews or this is not a valid product page"
+
     except:
         print "Unknown/Incorrect url! Please try with a different one"
         sys.exit()
