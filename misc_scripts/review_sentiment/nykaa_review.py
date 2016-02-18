dd5889e5be3a1ab1fcc8554520331ad0f84c6d99
vatsala <vatsala.swaroop@gmail.com>
2016-02-18 13:16:12 +0530

    Added comments

--- misc_scripts/review_sentiment/nykaa_review.py
+++ misc_scripts/review_sentiment/nykaa_review.py
@@ -5,19 +5,25 @@ import re
 import math
 from bs4 import BeautifulSoup as bs
 
+#tested largely on nykaa.com -> bath and body section
+#sample url : http://www.nykaa.com/bath-and-body/bath/showergels/neutrogena-rainbath-refreshing-shower-and-bath-gel.html?root=catg&ptype=product
+
 POSITIVE_FILE = "/home/vatsala/py-scripts/misc_scripts/review_sentiment/positive.txt"
 NEGATIVE_FILE = "/home/vatsala/py-scripts/misc_scripts/review_sentiment/negative.txt"
 
+#extracts review components from the product page
 def extract_reviews(soup):
     review_head = [str(review.findAll('p')[0].get_text()) for review in soup.findAll('div', class_ ='r-detail')]
     review_content = [str(review.findAll('p')[1].get_text()) for review in soup.findAll('div', class_ ='r-detail')]
     return {'review_head': review_head, 'review_content': review_content}
 
+#extracts overall rating for the product
 def extract_rating(soup):
     rating = soup.find('div', class_ ='product-shop')
     p_container = rating.find('p', class_ ='reviews')
     return str(p_container.find('meta')['content']) 
 
+#returns list of words in a file
 def get_file_data(filename):
     inFile = open(filename, 'r', 0)
     line = inFile.readline()
@@ -25,11 +31,13 @@ def get_file_data(filename):
     inFile.close()
     return wordlist
 
+#returns a map of positive and negative words
 def get_sentiment_list():   
     positive = get_file_data(POSITIVE_FILE)
     negative = get_file_data(NEGATIVE_FILE)
     return {'positive': positive , 'negative': negative}
 
+#computes sentiment for a given review component
 def compute_sentiment(review_component, negative_words, positive_words):
 
     positive = 0
@@ -46,7 +54,8 @@ def compute_sentiment(review_component, negative_words, positive_words):
                 if word.lower() in positive_words:
                     positive += 1
     return {'positive': positive, 'negative': negative}
-     
+
+#evaluates the final sentiment for the product    
 def find_sentiment(reviews, sentiment_map):
     
     negative_words = sentiment_map['negative']
@@ -66,6 +75,7 @@ def find_sentiment(reviews, sentiment_map):
     else:
         return "positive"
 
+#main function, calls all other functions and checks if input is valid
 if __name__ == '__main__':
 
     url = raw_input("\nEnter product url to get sentimental analysis based on recent reviews\n")
