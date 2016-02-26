82ad1d77b8fb8e39cf8b62cb0aaa749aba06f69e
vatsala <vatsala.swaroop@gmail.com>
2016-02-27 00:39:34 +0530

    Taking user input

--- zip.py
+++ zip.py
@@ -1,18 +1,28 @@
 import urllib2
 import json 
+import sys
+
+#sample values : 
+#country code - va , zipcode - 00120
+#country code - us , zipcode - 90210
+#country code - in , zipcode - 400021
 
 if __name__ == '__main__':
 
-	ccode = "IN"
-	zip_code ="440010"
-	url = "http://api.zippopotam.us/" + ccode + "/" + zip_code 
+	ccode = raw_input("\nEnter your country code\n")
+	zip_code = raw_input("\nEnter your zip code\n")
 
+	if not ccode or not zip_code:
+		print "You did not enter a value! Exiting...."
+		sys.exit()
+
+	url = "http://api.zippopotam.us/" + str(ccode).lower() + "/" + str(zip_code) 
+
+	
 	content = json.load(urllib2.urlopen(url))
-
 	if content:
-		print "You are located at "
+		print "\nYou may be around "
 		places = content['places']
 		for place in places:
 			print place['place name'] + ", " + place['state'] + ", " + content['country']
-	else:
-		print "Please try again with valid codes. Visit API to see supported countries"
+		print
