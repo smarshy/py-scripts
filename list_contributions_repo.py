4d1a716c40faf338fb67bae5771005821d29cc18
vatsala <vatsala.swaroop@gmail.com>
2016-02-27 16:47:20 +0530

    Modified list_contrributions for repo specific contributions

---
+++ list_contributions_repo.py
@@ -0,0 +1,54 @@
+import requests
+import sys
+
+def get_responses(url):
+
+	r = requests.get(url)
+	response = r.json()
+	return response['items']
+
+if __name__ == '__main__':
+
+	username = raw_input("\nEnter github username\n")
+	org = raw_input("\nEnter organization or owner\n")
+	repo = raw_input("\nEnter repository\n")
+	url = "https://api.github.com/search/issues?q=repo:" + org + "/" + repo + "+author:" + username + "+type:"
+
+	url1 = url + "issue"
+	url2 = url + "pr"
+
+	try:
+		issues = get_responses(url1)
+		prs = get_responses(url2)
+	except:
+		print "Check if entered details are valid/internet connectivity"
+		sys.exit()
+
+	print "\nContributions for user " + str(username) + " for repository " + org + "/" + repo
+	print "\nPull requests:\n"
+
+	for pr in prs:
+		print pr['html_url']
+	print
+
+	print "\nIssues:\n"
+
+	for issue in issues:
+		print issue['html_url']
+	print
+
+	
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
