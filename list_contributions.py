62d46c406e0587410d6da5d868b2e5fea85599ce
vatsala <vatsala.swaroop@gmail.com>
2016-02-27 16:17:29 +0530

    Added script to list contributions

---
+++ list_contributions.py
@@ -0,0 +1,52 @@
+import requests
+import sys
+
+def get_responses(url):
+
+	r = requests.get(url)
+	response = r.json()
+	return response['items']
+
+if __name__ == '__main__':
+
+	username = raw_input("\nEnter github username\n")
+	url = "https://api.github.com/search/issues?q=author:" + username + "+type:"
+
+	url1 = url + "issue"
+	url2 = url + "pr"
+
+	try:
+		issues = get_responses(url1)
+		prs = get_responses(url2)
+	except:
+		print "Check if username is valid/internet connection"
+		sys.exit()
+
+	print "\nContributions for user " + str(username)
+	print "\nPull requests:\n"
+
+	for pr in prs:
+		print pr['html_url']
+	print
+
+	print "\nIssues:\n"
+
+	for issue in issues:
+		print issue['html_url']
+	print
+
+	
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
