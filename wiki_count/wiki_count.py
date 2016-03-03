ae48acf2cf526a62dd0130f174c47bb6df0d2514
vatsala <vatsala.swaroop@gmail.com>
2016-03-03 22:03:24 +0530

    Getting list of stopwords

--- wiki_count/wiki_count.py
+++ wiki_count/wiki_count.py
@@ -1,6 +1,17 @@
+import string
 import re
 
 import wikipedia
+
+STOPWORD_FILE = "/home/vatsala/py-scripts/misc_scripts/wiki_count/stopwords.txt"
+
+#Loads and returns words from a custom stopword text file
+def get_stopWord_list():
+	inFile = open(STOPWORD_FILE, 'r', 0)
+	line = inFile.readline()
+	stopwordlist = string.split(line)
+	inFile.close()
+	return stopwordlist
 
 def main():
 	try:
@@ -8,7 +19,9 @@ def main():
 		page_name = raw_input("Enter a wikipedia page to analyse\n")
 		page = wikipedia.page(page_name)
 		details = {'Topic' : page.title, 'Url' : page.url, 'Number of links on the page' : len(page.links)}
+		stopwordlist = get_stopWord_list()
 		print details
+		print stopwordlist
 	except:
 		print "Invalid page entered or no internet connection"
 
